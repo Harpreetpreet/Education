203c5ab3e65c0b723e47a121ab475f9bbd422ee2
Harpreetpreet <preetx025@gmail.com>
2019-11-16 12:43:06 -0800

    add button

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,17 +6,41 @@ import Layout from '../components/Layout';
 
 const Section = styled.div`
   text-align: center;
-    height: 40rem;
-    background-position: center;
-    background-repeat: no-repeat;
-    background-size: cover;
   background: url("/images/home-banner.jpg");
-  }
-  .section{
-   
+  background-position: center;
+    background-repeat: no-repeat;
+    overflow: hidden;
+    min-height: 780px;
   }
   h1 {
     color: ${props => props.theme.mainBrandColor} !important;
+    font-size: 42px;
+    font-family: "Playfair Display", serif;
+    margin-bottom: 20px;
+}
+.text{
+  margin-top: 12rem;
+}
+  p{
+    font-size: 14px;
+    line-height: 24px;
+    font-family: "Roboto", sans-serif;
+    font-weight: normal;
+    max-width: 620px;
+    margin: auto;
+    color:#FFFFFF  !important;
+  }
+  button{
+    margin-top: 3rem;
+    font-size: 22px;
+    background-color:#F14668 !important;
+    color: white !important;
+    border-radius:5px solid black !important;
+    transition-duration: .5s;
+  }
+  .button:hover{
+    color:#F14668 !important;
+    background-color:transparent !important;
   }
 `;
 
@@ -27,9 +51,18 @@ export default class IndexPage extends React.Component {
         <Seo title="Home" description="Welcome to GatsbyJs v1" />
 
         <Section className="section">
-          <h1 className="title">We Ensure better education</h1>
-          <p>Welcome to your new Gatsby site.</p>
-          <p>Now go build something great.</p>
+          <div className="text">
+          <h1 className="title">
+            We Ensure better education
+            <h1 className="title">for a better world</h1>
+          </h1>
+          </div>
+          <p>
+            In the history of modern astronomy, there is probably no one greater
+            leap forward than the building
+            <span>and launch of the space telescope known as the Hubble.</span>
+          </p>
+          <button class="button is-danger is-outlined">Get Started</button>
         </Section>
       </Layout>
     );
